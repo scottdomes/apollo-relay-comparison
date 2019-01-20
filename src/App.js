6fef4ac235f30b7a67323065e14c905cb3d4d5f6
scottdomes <scott@musefind.com>
2019-01-19 20:16:32 -0800

    Apollo write to cache

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import get from 'lodash/get';
-import { ApolloProvider, Query, Mutation } from 'react-apollo';
+import { ApolloProvider, ApolloConsumer, Query, Mutation } from 'react-apollo';
 
 import gql from 'graphql-tag';
 import './App.css';
@@ -56,62 +56,84 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <div className="App">
-          <Query query={GET_CONTACTS}>
-            {({ data, loading, error, refetch }) => {
-              if (loading) return <p>Loading...</p>;
-              if (error) return <p>ERROR</p>;
+        <ApolloConsumer>
+          {client => (
+            <div className="App">
+              <Query query={GET_CONTACTS}>
+                {({ data, loading, error, refetch }) => {
+                  if (loading) return <p>Loading...</p>;
+                  if (error) return <p>ERROR</p>;
 
-              const contacts = get(data, 'viewer.allContacts.edges', []);
+                  const contacts = get(data, 'viewer.allContacts.edges', []);
 
-              return (
-                <div>
-                  {contacts.map(edge => {
-                    const contact = edge.node;
-                    return (
-                      <div key={contact.id}>
-                        <p>Name: {contact.name}</p>
-                        <p>Email: {contact.email}</p>
-                      </div>
-                    );
-                  })}
-                  <Mutation
-                    mutation={CREATE_CONTACT}
-                    onCompleted={() => refetch()}>
-                    {(create, { data }) => (
-                      <form
-                        onSubmit={e => {
-                          e.preventDefault();
-                          create({
-                            variables: {
-                              input: {
-                                name: this.state.name,
-                                email: this.state.email
+                  return (
+                    <div>
+                      {contacts.map(edge => {
+                        const contact = edge.node;
+                        return (
+                          <div key={contact.id}>
+                            <p>Name: {contact.name}</p>
+                            <p>Email: {contact.email}</p>
+                          </div>
+                        );
+                      })}
+                      <Mutation
+                        mutation={CREATE_CONTACT}
+                        update={(cache, { data: { createContact } }) => {
+                          const oldContacts = cache.readQuery({
+                            query: GET_CONTACTS
+                          }).viewer.allContacts.edges;
+                          cache.writeQuery({
+                            query: GET_CONTACTS,
+                            data: {
+                              viewer: {
+                                __typename: 'Viewer',
+                                allContacts: {
+                                  __typename: 'ContactConnection',
+                                  edges: oldContacts.concat([
+                                    createContact.contactEdge
+                                  ])
+                                }
                               }
                             }
                           });
                         }}>
-                        <label>Name</label>
-                        <input
-                          type="text"
-                          value={this.state.name}
-                          onChange={this.handleNameChange}
-                        />
-                        <label>Email</label>
-                        <input
-                          type="text"
-                          value={this.state.email}
-                          onChange={this.handleEmailChange}
-                        />
-                        <button type="submit">Add Contact</button>
-                      </form>
-                    )}
-                  </Mutation>
-                </div>
-              );
-            }}
-          </Query>
-        </div>
+                        {(create, { data }) => (
+                          <form
+                            onSubmit={e => {
+                              e.preventDefault();
+                              create({
+                                variables: {
+                                  input: {
+                                    name: this.state.name,
+                                    email: this.state.email
+                                  }
+                                }
+                              });
+                            }}>
+                            <label>Name</label>
+                            <input
+                              type="text"
+                              value={this.state.name}
+                              onChange={this.handleNameChange}
+                            />
+                            <label>Email</label>
+                            <input
+                              type="text"
+                              value={this.state.email}
+                              onChange={this.handleEmailChange}
+                            />
+                            <button type="submit">Add Contact</button>
+                          </form>
+                        )}
+                      </Mutation>
+                    </div>
+                  );
+                }}
+              </Query>
+            </div>
+          )}
+        </ApolloConsumer>
       </ApolloProvider>
     );
   }
